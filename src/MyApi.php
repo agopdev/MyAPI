533de5604c98c1b04aab7bdeefe34b1933ae4c96
agopdev <info@agop.dev>
2024-05-04 17:03:27 -0600

    Corrected errors in class

--- src/MyApi.php
+++ src/MyApi.php
@@ -2,112 +2,112 @@
 
 class MyApi {
 
-    private static $root;
-    private static $uri;
-    private static $routesController;
-    private static $api;
-    private static $req;
+private static $root;
+private static $uri;
+private static $routesController;
+private static $api;
+private static $req;
 
-    private function __construct($routesController){
+private function __construct($routesController){
 
-        $this->root = $_SERVER['DOCUMENT_ROOT'];
-        define("URL_CONTROLLERS", "{$this->root}/src/routes/bases");
+    self::$root = $_SERVER['DOCUMENT_ROOT'];
+    define("URL_CONTROLLERS", self::$root . "/src/routes/bases");
 
-        $this->uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $this->uri = explode('/', $this->uri);
+    self::$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    self::$uri = explode('/', self::$uri);
 
-        $this->routesController = $routesController;
+    self::$routesController = $routesController;
 
+}
+
+private static function loadInstance($routesController){
+    if (empty(self::$api)){
+        self::$api = new MyApi($routesController);
+    }
+}
+
+private static function getController(){
+    $requested_uri = self::$uri[1];
+    $controller = URL_CONTROLLERS;
+
+    if (array_key_exists($requested_uri, self::$routesController)) {
+        $controllerName =  self::$routesController[$requested_uri];
+        $controller .= "/{$controllerName}.php";
+    } else {
+        http_response_code(404);
+        return;
     }
 
-    private static function loadInstance($routesController){
-        if (empty(self::$api)){
-            self::$api = new MyApi($routesController);
+    return $controller;
+}
+
+private static function callRoute($controller){
+
+    if (http_response_code() === 404){
+        return;
+    }
+
+    $server_http_method = $_SERVER['REQUEST_METHOD'];
+    $requested_uri = self::$uri[1];
+    $controller = self::$routesController[$requested_uri];
+    $routes = constant("$controller::routes");
+    $sliced_uri = array_slice(self::$uri, 1);
+    $requested_uri = '/' . implode('/', $sliced_uri);
+    $requested_uri = (substr($requested_uri, -1) === '/') ? rtrim($requested_uri, '/') : $requested_uri;
+    $requested_uri_format = $server_http_method . '@' . $requested_uri;
+    $controller_method = null;
+
+    foreach ($routes as $pattern => $route){
+        if (preg_match("#^$pattern$#", $requested_uri_format)){
+            $controller_method = $route;
+            break;
         }
     }
+
+    if (empty($controller_method)){
+        http_response_code(404);
+        return;
+    }
+
+    $req_params = null;
+    $req_body = null;
+
+    $req_params = self::$uri;
+
+    $body = file_get_contents('php://input');
+
+    if (!empty($body)) {
+        $json_body = file_get_contents('php://input');
+        $req_body = json_decode($json_body, true);
+    }
+
+    self::$req = new Request($req_params, $req_body);
+
+    return $controller::$controller_method(self::$req);
+}
+
+public static function handleCalls($routesController){
+    self::loadInstance($routesController);
+
+    $controller = self::getController();
     
-    private static function getController(){
-        $requested_uri = self::$api->uri[1];
-        $controller = URL_CONTROLLERS;
-
-        if (array_key_exists($requested_uri, self::$api->routesController)) {
-            $controllerName =  self::$api->routesController[$requested_uri];
-            $controller .= "/{$controllerName}.php";
-        } else {
-            http_response_code(404);
-            return;
-        }
-
-        return $controller;
+    if (self::callRoute($controller) === false) {
+        http_response_code(500);
+        return;
     }
-
-    private static function callRoute($controller){
-
-        if (http_response_code() === 404){
-            return;
-        }
-
-        $server_http_method = $_SERVER['REQUEST_METHOD'];
-        $requested_uri = self::$api->uri[1];
-        $controller = self::$api->routesController[$requested_uri];
-        $routes = constant("$controller::routes");
-        $sliced_uri = array_slice(self::$api->uri, 1);
-        $requested_uri = '/' . implode('/', $sliced_uri);
-        $requested_uri = (substr($requested_uri, -1) === '/') ? rtrim($requested_uri, '/') : $requested_uri;
-        $requested_uri_format = $server_http_method . '@' . $requested_uri;
-        $controller_method = null;
-
-        foreach ($routes as $pattern => $route){
-            if (preg_match("#^$pattern$#", $requested_uri_format)){
-                $controller_method = $route;
-                break;
-            }
-        }
-
-        if (empty($controller_method)){
-            http_response_code(404);
-            return;
-        }
-
-        $req_params = null;
-        $req_body = null;
-
-        $req_params = self::$api->uri;
-
-        $body = file_get_contents('php://input');
-    
-        if (!empty($body)) {
-            $json_body = file_get_contents('php://input');
-            $req_body = json_decode($json_body, true);
-        }
-
-        self::$req = new Request($req_params, $req_body);
-
-        return $controller::$controller_method(self::$req);
-    }
-
-    public static function handleCalls($routesController){
-        self::loadInstance($routesController);
-
-        $controller = self::getController();
-        
-        if (self::callRoute($controller) === false) {
-            http_response_code(500);
-            return;
-        }
-    }
+}
 
 }
 
 class Request {
 
-    public static $params;
-    public static $body;
+public $params;
+public $body;
 
-    public function __construct($params, $body){
+public function __construct($params, $body){
 
-        $this->params = $params;
-        $this->body = $body;
+    $this->params = $params;
+    $this->body = $body;
 
-    }
+}
 }
